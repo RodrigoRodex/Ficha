593f405e995170b535fbf10cfec99409bb256350
Rodrigo <rodex97@gmail.com>
2021-08-04 16:35:56 -0300

    Dado com atributo/pronto

--- GUI/GUI3.py
+++ GUI/GUI3.py
@@ -1,10 +1,9 @@
-#   from class_GUi import Num_Entry
 from tkinter import Button, Entry, Label, Tk
-from tkinter.constants import DISABLED, W
+from tkinter.constants import DISABLED
 import random
 
 
-def callback(input):
+def volta(input):
     #   isdigit conta -, +, *, /, então não da pra colocar nas Entrys
     if input.isdigit():
         print(input)
@@ -19,143 +18,115 @@ def callback(input):
         return False
 
 
-window = Tk()
-reg = window.register(callback)
+Janela = Tk()
 
+reg = Janela.register(volta)
 largura_App = 400
 altura_App = 350
 
-largura_Da_Tela = window.winfo_screenwidth()
-altura_Da_Tela = window.winfo_screenheight()
+largura_Da_Tela = Janela.winfo_screenwidth()
+altura_Da_Tela = Janela.winfo_screenheight()
 
 posX = largura_Da_Tela/2 - largura_App/2
 posY = altura_Da_Tela/2 - altura_App/2
 
-window.title("Titulo")
-window.geometry("%dx%d+%d+%d" % (largura_App, altura_App, posX, posY))
-#   isso faz o GUI aparecer no meio da tela
+Janela.title("Titulo")
+Janela.geometry("%dx%d+%d+%d" % (largura_App, altura_App, posX, posY))
 
-#   procurar como colocar uma lista e escolher o número
-Label(window, text="Força:").grid(row=0, sticky=W)
-En_For = Entry(window)
-En_For.grid(row=0, column=1)
-En_For.config(validate="key",
-              validatecommand=(reg, '%P'))
+En_For = Entry(Janela)
+En_Car = Entry(Janela)
+En_Const = Entry(Janela)
+En_Inte = Entry(Janela)
+En_Dex = Entry(Janela)
+En_Sab = Entry(Janela)
+
+Text_For = "Contar a Força"
+Text_Car = "Contar o Carisma"
+Text_Const = "Contar a Constituição"
+Text_Inte = "Contar a Inteligência"
+Text_Dex = "Contar a Destreza"
+Text_Sab = "Contar a Sabedoria"
 
 
 def Força():
     En_For.config(state=DISABLED)
-    #   o disable serve pra travar e não deixar a pessoa mudar o
-    #   número das habilidades
     num = random.randint(1, 20)
-    For_enntry = int(En_For.get())
-    Soma = (num + For_enntry)
-    Label(window, text=Soma).grid(row=0, column=2)
-
-
-Button(window,
-       text="Contar a Força",
-       command=Força).grid(row=1, column=1)
-
-
-Label(window, text="Carisma:").grid(row=2, sticky=W)
-En_Car = Entry(window)
-En_Car.grid(row=2, column=1)
-En_Car.config(validate="key",
-              validatecommand=(reg, '%P'))
+    Entrada_For = int(En_For.get())
+    Soma = (num + Entrada_For)
+    Label(Janela,
+          text=Soma).grid(row=1, column=4)
 
 
 def Carisma():
     En_Car.config(state=DISABLED)
     num = random.randint(1, 20)
-    Car_Entry = int(En_Car.get())
-    Soma = (num + Car_Entry)
-    Label(window, text=Soma).grid(row=2, column=2)
-
-
-Button(window,
-       text="Contar o Carisma",
-       command=Carisma).grid(row=3, column=1)
-
-Label(window, text="Constituição:").grid(row=4, sticky=W)
-En_Const = Entry(window)
-En_Const.grid(row=4, column=1)
-En_Const.config(validate="key",
-                validatecommand=(reg, '%P'))
+    Entrada_Car = int(En_Car.get())
+    Soma = (num + Entrada_Car)
+    Label(Janela,
+          text=Soma).grid(row=2, column=4)
 
 
 def Constituição():
     En_Const.config(state=DISABLED)
     num = random.randint(1, 20)
-    Const_Entry = int(En_Const.get())
-    Soma = (num + Const_Entry)
-    Label(window,
-          text=Soma).grid(row=4, column=2)
-
-
-Button(window,
-       text="Contar o Constituiçao",
-       command=Constituição).grid(row=5, column=1)
-
-Label(window, text="Inteligência:").grid(row=6, sticky=W)
-En_Inte = Entry(window)
-En_Inte.grid(row=6, column=1)
-En_Inte.config(validate="key",
-               validatecommand=(reg, '%P'))
+    Entrada_Const = int(En_Const.get())
+    Soma = (num + Entrada_Const)
+    Label(Janela,
+          text=Soma).grid(row=3, column=4)
 
 
 def Inteligência():
     En_Inte.config(state=DISABLED)
     num = random.randint(1, 20)
-    Inte_Entry = int(En_Inte.get())
-    Soma = (Inte_Entry + num)
-    Label(window,
-          text=Soma).grid(row=6, column=2)
-
-
-Button(window,
-       text="Contar a Inteligência",
-       command=Inteligência).grid(row=7, column=1)
-
-#   ta falhando por causa do callback???
-Label(window, text="Destreza:").grid(row=9, sticky=W)
-En_Dex = Entry(window)
-En_Dex.grid(row=9, column=1)
-En_Dex.config(validate="key",
-              validatecommand=(reg, '%P'))
+    Entrada_Inte = int(En_Inte.get())
+    Soma = (num + Entrada_Inte)
+    Label(Janela,
+          text=Soma).grid(row=4, column=4)
 
 
 def Destreza():
     En_Dex.config(state=DISABLED)
     num = random.randint(1, 20)
-    Dex_Entry = int(En_Dex.get())
-    Soma = (Dex_Entry + num)
-    Label(window,
-          text=Soma).grid(row=9, column=2)
-
-
-Button(window,
-       text="Contar a Destreza",
-       command=Destreza).grid(row=10, column=1)
-
-
-Label(window, text="Sabedoria:").grid(row=11, sticky=W)
-En_Sab = Entry(window)
-En_Sab.grid(row=11, column=1)
-En_Sab.config(validate="key",
-              validatecommand=(reg, '%P'))
+    Entrada_Dex = int(En_Dex.get())
+    Soma = (num + Entrada_Dex)
+    Label(Janela,
+          text=Soma).grid(row=5, column=4)
 
 
 def Sabedoria():
-    En_Sab.config(state=(DISABLED))
+    En_Sab.config(state=DISABLED)
     num = random.randint(1, 20)
-    Sab_Entry = int(En_Sab.get())
-    Soma = (Sab_Entry + num)
-    Label(window,
-          text=Soma).grid(row=11, column=2)
+    Entrada_Sab = int(En_Sab.get())
+    Soma = (num + Entrada_Sab)
+    Label(Janela,
+          text=Soma).grid(row=6, column=4)
 
 
-Button(window,
-       text="Contar Sabedoria",
-       command=Sabedoria).grid(row=12, column=1)
-window.mainloop()
+Atributos = [("Força:", En_For, 1, 1, 1, 2),
+             ("Carisma:", En_Car, 2, 1, 2, 2),
+             ("Constituição:", En_Const, 3, 1, 3, 2),
+             ("Inteligência:", En_Inte, 4, 1, 4, 2),
+             ("Destreza:", En_Dex, 5, 1, 5, 2),
+             ("Sabedoria:", En_Sab, 6, 1, 6, 2)]
+
+valores = [(1, 3, Text_For, Força),
+           (2, 3, Text_Car, Carisma),
+           (3, 3, Text_Const, Constituição),
+           (4, 3, Text_Inte, Inteligência),
+           (5, 3, Text_Dex, Destreza),
+           (6, 3, Text_Sab, Sabedoria)]
+
+for name, En_name, r_val, c_val, r_val2, c_val2 in Atributos:
+    Label(Janela,
+          text=name,
+          padx=20).grid(row=r_val, column=c_val)
+    En_name.config(validate="key",
+                   validatecommand=(reg, '%P'))
+    En_name.grid(row=r_val2, column=c_val2)
+
+for r_val3, c_val3, Texto, fun in valores:
+    Button(Janela,
+           text=Texto,
+           command=fun).grid(row=r_val3, column=c_val3)
+
+Janela.mainloop()
